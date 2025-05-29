c05ffb36d0b5cd95234c045f8803b957d190e6a0
Stefan <stefan97@yahoo.com>
2025-05-29 13:21:11 +0200

    Stop tracking dist-electron

--- main-app/dist-electron/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("api", {
-    print: function () { return electron_1.ipcRenderer.invoke("print"); },
-});
