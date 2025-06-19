fb533f4cc8e393c583b1dbf910238d54a46840e9
Stefan Banovic <sbanovic87@gmail.com>
2025-06-19 21:35:37 +0200

    VCV-general: minor fixes

--- main-app/src/pages/welcomePage.tsx
+++ main-app/src/pages/welcomePage.tsx
@@ -1,7 +1,5 @@
 import { useNavigate } from 'react-router-dom'
 
-import Footer from '../components/organisms/footer/footer'
-import Header from '../components/organisms/header/header'
 import Welcome from '../components/templates/welcome/welcomeTemplate'
 
 export default function WelcomePage() {
