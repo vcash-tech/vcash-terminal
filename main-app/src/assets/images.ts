9e12961d295e890202041597becbf2fe78f2fa7f
Predrag Jevtic <designbyheart@gmail.com>
2025-07-01 05:29:17 +0200

    Added home page with transition for screen saver

--- main-app/src/assets/images.ts
+++ main-app/src/assets/images.ts
@@ -1,7 +1,10 @@
 import ageDisclaimerBackground from './images/ageDisclaimerBackground.svg'
+import basketballCard from './images/basketballCard.svg'
 import bettingServices from './images/bettingServices.png'
 import cash from './images/cash.png'
 import cashPayment from './images/cashPayment.svg'
+import casinoCard from './images/casinoCard.png'
+import casinoCard2 from './images/casinoCard2.png'
 import creditCard from './images/creditCard.png'
 import creditCardPayment from './images/creditCardPayment.svg'
 import digitalServices from './images/digitalServices.png'
@@ -9,23 +12,32 @@ import disney from './images/disney.png'
 import eaPlay from './images/eaPlay.png'
 import emailCompleted from './images/emailCompleted.svg'
 import emailInputBackground from './images/emailInputBackground.svg'
+import f1Card from './images/f1Card.png'
+import gamingServices from './images/gamingServices.png'
 import gPay from './images/googlePay.png'
 import hbo from './images/hbo.png'
 import homeBackground from './images/homeBackground.png'
 import hulu from './images/hulu.png'
 import insertCash from './images/insertCash.svg'
+import logoWhite from './images/logoWhite.svg'
 import paymentCardBackground from './images/paymentCardBackground.svg'
 import paymentMethodBackground from './images/paymentMethodBackground.png'
+import playstationCard from './images/playstationCard.png'
 import ps from './images/ps.png'
 import qrCode from './images/qrCode.svg'
+import sockerCard from './images/sockerCard.svg'
 import steam from './images/steam.png'
+import steamCard from './images/steamCard.png'
 import welcomeBackground from './images/welcomeBackground.svg'
+import xboxCard from './images/xboxCard.png'
 
 export {
     ageDisclaimerBackground,
+    basketballCard,
     bettingServices,
     cash,
     cashPayment,
+    casinoCard, casinoCard2,
     creditCard,
     creditCardPayment,
     digitalServices,
@@ -33,14 +45,22 @@ export {
     eaPlay,
     emailCompleted,
     emailInputBackground,
+    f1Card,
+    gamingServices,
     gPay,
     hbo,
     homeBackground,
     hulu,
     insertCash,
+    logoWhite,
     paymentCardBackground,
     paymentMethodBackground,
+    playstationCard,
     ps,
     qrCode,
+    sockerCard,
     steam,
-    welcomeBackground}
+    steamCard,
+    welcomeBackground,
+    xboxCard
+}
