63e34a7d63e1686180906c5ae17f097d2c9bff7b
Stefan Banovic <sbanovic87@gmail.com>
2025-07-08 15:04:52 +0200

    VCV-final: Minor changes to text and positions

--- main-app/src/components/atoms/primaryButton/primaryButton.tsx
+++ main-app/src/components/atoms/primaryButton/primaryButton.tsx
@@ -2,15 +2,18 @@ export default function PrimaryButton({
     text,
     callback,
     onPress,
-    inverted
+    inverted,
+    isDisabled,
 }: {
     text: string
     callback?: () => void
     inverted?: boolean
     onPress?: () => void
+    isDisabled?: boolean
 }) {
     return (
         <button
+            disabled={isDisabled}
             className={`primary-button ${inverted ? 'inverted' : ''}`}
             onClick={callback || onPress}>
             <span>{text}</span>
