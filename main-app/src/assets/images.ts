ccc97a9db8eb176903b59133de81f848564d0a75
Predrag Jevtic <designbyheart@gmail.com>
2025-07-09 13:54:47 +0200

    Updated images

--- main-app/src/assets/images.ts
+++ main-app/src/assets/images.ts
@@ -1,12 +1,13 @@
 import ageDisclaimerBackground from './images/ageDisclaimerBackground.webp'
 import background from './images/background.webp'
 import balkanBetLogo from './images/balkanBetLogo.webp'
-import bettingServices from './images/bettingServices.png'
+import bettingServices from './images/bettingServices.webp'
 import betVoucher from './images/betVoucher.webp'
 import cash from './images/cash.webp'
 import casinoNewCard from './images/casinoNew.webp'
 import chechLights from './images/chechLights.webp'
 import comingSoonBackground from './images/coming-soon-background.webp'
+import creditCard from './images/creditCard.webp'
 import emailCompleted from './images/emailCompleted.webp'
 import emailInputBackground from './images/emailInputBackground.webp'
 import footballCard from './images/footballCard.webp'
@@ -21,7 +22,7 @@ import nbaCard from './images/nbaCard.webp'
 import paymentCardBackground from './images/paymentCardBackground.webp'
 import paymentMethodBackground from './images/paymentMethodBackground.webp'
 import playStationCard from './images/playstationCard-no-price.webp'
-import playstationClean from './images/playstationCardClean.webp'
+import playStationCardClean from './images/playstationCardClean.webp'
 import printVoucher from './images/printVoucher.webp'
 import ps from './images/ps.webp'
 import qrCode from './images/qrCode.webp'
@@ -45,6 +46,7 @@ export {
     casinoNewCard,
     chechLights,
     comingSoonBackground,
+    creditCard,
     emailCompleted,
     emailInputBackground,
     footballCard,
@@ -60,7 +62,7 @@ export {
     paymentCardBackground,
     paymentMethodBackground,
     playStationCard,
-    playstationClean,
+    playStationCardClean,
     printVoucher,
     ps,
     qrCode,
