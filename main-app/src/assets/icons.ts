dda2d1837fefe7d32a927fa9b79cfda7d73c218b
Predrag Jevtic <designbyheart@gmail.com>
2025-07-20 11:10:21 +0200

    age disclaimer

--- main-app/src/assets/icons.ts
+++ main-app/src/assets/icons.ts
@@ -3,6 +3,7 @@ import ageDisclaimer from './icons/ageDisclaimer.svg'
 import ageDisclaimerWhite from './icons/ageDisclaimerWhite.svg'
 import angleLeft from './icons/angleLeft.svg'
 import arrowRight from './icons/arrowRight.svg'
+import bettingWarning from './icons/betttingWarning.svg'
 import closeIcon from './icons/closeIcon.svg'
 import comingSoon from './icons/coming-soon.svg'
 import comingSoonGaming from './icons/coming-soon-gaming.svg'
@@ -12,9 +13,12 @@ import dashLine from './icons/dash-line.svg'
 import deviceIcon from './icons/deviceIcon.svg'
 import flagEN from './icons/flagEN.svg'
 import flagRS from './icons/flagRS.svg'
+import hospitalLogo from './icons/hospitalLogo.svg'
 import infoCircle from './icons/infoCircle.svg'
 import paymentsIcon from './icons/paymentsIcon.svg'
 import printerUnavailable from './icons/printerUnavailable.svg'
+import redWarningIcon from './icons/redWarningIcon.svg'
+import serbiaLogo from './icons/serbiaLogo.svg'
 import sessionExpired from './icons/sessionExpired.svg'
 import ukIcon from './icons/ukIcon.svg'
 import userIcon from './icons/userIcon.svg'
@@ -29,6 +33,7 @@ export {
     ageDisclaimerWhite,
     angleLeft,
     arrowRight,
+    bettingWarning,
     closeIcon,
     comingSoon,
     comingSoonGaming,
@@ -38,9 +43,12 @@ export {
     deviceIcon,
     flagEN,
     flagRS,
+    hospitalLogo,
     infoCircle,
     paymentsIcon,
     printerUnavailable,
+    redWarningIcon,
+    serbiaLogo,
     sessionExpired,
     ukIcon,
     userIcon,
