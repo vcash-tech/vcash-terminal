fabc10f79766bee8cf597758639bc3d51afd89be
Stefan Banovic <sbanovic87@gmail.com>
2025-07-30 19:54:58 +0200

    Return to browserRouter instead of memoryRouter

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -1,6 +1,6 @@
 import './styles/app.scss'
 
-import { MemoryRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import BettingPage from '@/pages/bettingPage'
 import GamingPage from '@/pages/gamingPage'
