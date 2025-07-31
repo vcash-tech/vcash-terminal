903aa6ec58598b94beb09cb77745884b8280f0a5
Stefan Banovic <sbanovic87@gmail.com>
2025-07-31 12:38:23 +0200

    Additional release fixes

--- main-app/src/components/templates/voucherDataError/VoucherErrorTemplate.tsx
+++ main-app/src/components/templates/voucherDataError/VoucherErrorTemplate.tsx
@@ -3,11 +3,11 @@ import { NavigateFunction } from 'react-router-dom'
 
 import { printVoucher } from '@/assets/images'
 import Container from '@/components/atoms/container/container'
+import PrimaryButton from '@/components/atoms/primaryButton/primaryButton'
 import WireButton from '@/components/atoms/wireButton/wireButton'
 import Footer from '@/components/organisms/footer/footer'
 import Header from '@/components/organisms/header/header'
 import { useTranslate } from '@/i18n/useTranslate'
-import PrimaryButton from '@/components/atoms/primaryButton/primaryButton'
 
 export default function VoucherErrorTemplate({
     navigate,
