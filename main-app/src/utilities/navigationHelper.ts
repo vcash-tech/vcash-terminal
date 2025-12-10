62b3b19ffb2a5def0f030e2aee10ffa1c452789f
Milos Spasojevic <milos.s.pfc@gmail.com>
2025-12-10 21:04:01 +0100

    hide connectivity modal on connectivity page

--- main-app/src/utilities/navigationHelper.ts
+++ main-app/src/utilities/navigationHelper.ts
@@ -1,3 +1,7 @@
 export const isHomePage = (path: string) => {
     return ['/welcome', '/welcome-with-services'].includes(path)
 }
+
+export const isConnectivityIssuesPage = (path: string) => {
+    return ['/connectivity-issues'].includes(path)
+}
