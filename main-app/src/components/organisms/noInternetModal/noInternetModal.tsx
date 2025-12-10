3224bfdfbf327eadbfbd34ab5005d51503c1dbc8
Milos Spasojevic <milos.s.pfc@gmail.com>
2025-12-10 21:04:01 +0100

    hide connectivity modal on connectivity page

--- main-app/src/components/organisms/noInternetModal/noInternetModal.tsx
+++ main-app/src/components/organisms/noInternetModal/noInternetModal.tsx
@@ -3,14 +3,21 @@ import { useLocation } from 'react-router-dom'
 import AlertModal from '@/components/organisms/alertModal/alertModal'
 import { useCheckInternetConnection } from '@/hooks'
 import { useTranslate } from '@/i18n/useTranslate'
-import { isHomePage } from '@/utilities/navigationHelper'
+import {
+    isConnectivityIssuesPage,
+    isHomePage
+} from '@/utilities/navigationHelper'
 
 export default function NoInternetModal() {
     const { t } = useTranslate()
     const { isOnline, isMoneyPending } = useCheckInternetConnection()
     const { pathname } = useLocation()
 
-    if (isHomePage(pathname) || isOnline) {
+    if (
+        isHomePage(pathname) ||
+        isConnectivityIssuesPage(pathname) ||
+        isOnline
+    ) {
         return <></>
     }
 
